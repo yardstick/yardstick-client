43815f6169f7df341705a48a839d92b7d33f108a
Jason Wall <javajo@gmail.com>
2013-07-05 14:35:29 -0600

    initial gem structure

---
+++ lib/yardstick-client.rb
@@ -0,0 +1,7 @@
+require "yardstick-client/version"
+
+module Yardstick
+  module Client
+    # Your code goes here...
+  end
+end
---
+++ lib/yardstick-client/version.rb
@@ -0,0 +1,5 @@
+module Yardstick
+  module Client
+    VERSION = "0.0.1"
+  end
+end
