d12c028e77df458a8c80e835cb7bb774ce592600
Jason Wall <javajo@gmail.com>
2013-07-08 15:36:51 -0600

    Port in the api stub helpers

---
+++ lib/yardstick/v2_client/testing.rb
@@ -0,0 +1,8 @@
+require 'yardstick/v2_client/testing/api_stub_helpers'
+
+module Yardstick
+  module V2Client
+    module Testing
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,18 @@
 require 'active_support'
 require 'active_model'
 require 'yardstick-client'
+
+require 'mocha/api'
+require 'yardstick/v2_client/testing'
+
+RSpec.configure do |config|
+  config.mock_with :mocha
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+
+  config.order = "random"
+
+  config.include Yardstick::V2Client::Testing::ApiStubHelpers
+end
