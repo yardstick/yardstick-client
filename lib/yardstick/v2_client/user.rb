13cd05b9464db2943587b0027c34d700662f9bed
Jason Wall <javajo@gmail.com>
2014-10-24 15:30:21 -0600

    add missing user attributes

--- lib/yardstick/v2_client/user.rb
+++ lib/yardstick/v2_client/user.rb
@@ -8,6 +8,17 @@ module Yardstick
       include Cacheable
 
       cached_accessor :id, :username, :first_name, :last_name, :email, :roles
+
+      attr_accessor :metadata,
+                    :active,
+                    :site_id,
+                    :created_at,
+                    :alternate_email_verified,
+                    :alternate_email,
+                    :email_verified,
+                    :deleted_at,
+                    :updated_at,
+                    :address
     end
   end
 end
