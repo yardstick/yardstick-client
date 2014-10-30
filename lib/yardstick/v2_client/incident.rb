af4b6de601fcc532c474260c5d50b229737d19d0
Jason Wall <javajo@gmail.com>
2014-10-30 15:13:16 -0600

    add forgotten incident type field to model

--- lib/yardstick/v2_client/incident.rb
+++ lib/yardstick/v2_client/incident.rb
@@ -5,7 +5,7 @@ module Yardstick
 
       resource_uri '/v2/incidents'
 
-      attr_accessor :id, :source_id, :source_type, :created_at, :updated_at, :notes
+      attr_accessor :id, :source_id, :source_type, :created_at, :updated_at, :incident_type, :notes
       attr_accessor :incident_datetime
       attr_accessor :proctor_ids, :proctors, :user_ids, :users
       attr_accessor :reporting_proctor_id, :reporting_proctor
