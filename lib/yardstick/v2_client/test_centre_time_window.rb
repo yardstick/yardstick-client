634eb10d8a6ae4f97148a362beb11a98e08dc5e3
Alecia Vogel <hello@alecia.ca>
2018-05-10 15:34:29 -0600

    alias_method fail 2: electric boogaloo

--- lib/yardstick/v2_client/test_centre_time_window.rb
+++ lib/yardstick/v2_client/test_centre_time_window.rb
@@ -17,7 +17,7 @@ module Yardstick
       attr_accessor :incident_ids, :incidents, :test_centre_seats
       attr_accessor :paths
 
-      alias_method :source_id, :id
+      alias_method :id, :source_id
 
       def local_start_datetime
         global_start_datetime.in_time_zone(ActiveSupport::TimeZone[time_zone])
