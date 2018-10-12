5fac0903965048a7c7f6db003661d50c1296cbf8
Josh Burman <josh.burman@getyardstick.com>
2018-10-12 14:22:12 -0400

    updated getting all grants

--- lib/yardstick-client/version.rb
+++ lib/yardstick-client/version.rb
@@ -1,5 +1,9 @@
 module Yardstick
   module Client
+<<<<<<< Updated upstream
     VERSION = '0.2.6'
+=======
+    VERSION = '0.2.8'.freeze
+>>>>>>> Stashed changes
   end
 end
---
+++ lib/yardstick/v2_client/grant.rb
@@ -0,0 +1,46 @@
+require 'yardstick/v2_client/remote_model'
+require 'yardstick/cacheable'
+
+module Yardstick
+  module V2Client
+    class Grant
+      include RemoteModel
+      include Cacheable
+
+      cached_accessor :id
+
+      attr_accessor :metadata,
+                    :start_at,
+                    :expire_at,
+                    :allow_sittings_start_at,
+                    :allow_sittings_end_at,
+                    :time_zone_name,
+                    :exam_form_id,
+                    :send_email,
+                    :exam_form_group,
+                    :num_uses
+
+      resource_uri '/v2/users'
+
+      def self.find(token, user_id, id)
+        from_api(get("#{resource_uri}/#{user_id}/grants/#{id}", query: { token: token }), token: token)
+      end
+
+      def self.all(token, user_id, options = {})
+        uri = "#{resource_uri}/#{user_id}/grants"
+        query_collection(token, uri, options)
+      end
+
+      def update(params)
+        self.response = put(
+          instance_uri,
+          body: {
+            token: token,
+            grant: params
+          }
+        )
+        self
+      end
+    end
+  end
+end
